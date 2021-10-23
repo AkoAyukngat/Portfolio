8692e208bbf9a9ad260744b30b3fa6d6ad330e44
AkoAyukngat <johnako64@gmail.com>
2021-10-23 08:08:47 +0200

    empty changes on contactpage

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Contact = () => {
-	return <div className="contact_me">Contact</div>;
+  return <div className="contact_me">Contact</div>;
 };
 
 export default Contact;
